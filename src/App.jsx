3a42db1489cd6fc4121f2cd9020c1e7ef4c53d46
Richie735 <Richie735@users.noreply.github.com>
2022-12-02 09:37:07 +0000

    General Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Header from "./components/Header";
+import { About, Contact, Experience, Footer, Nav } from "./components/index";
 
 const App = () => {
   return (
     <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 };
 
 export default App;
