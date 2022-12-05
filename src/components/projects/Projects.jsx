1168d2836fba24def7a179884eec7b4f33f2cc7f
Richie735 <Richie735@users.noreply.github.com>
2022-12-05 15:38:05 +0000

    Projetcs - notResponsive

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -39,38 +39,69 @@ const projects = [
     links: "https://github.com/Richie735",
     image: require("../../assets/portfolio3.jpg"),
   },
+  {
+    name: "Project 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti impedit, amet odit tenetur obcaecati distinctio veniam suscipit dignissimos culpa natus assumenda ratione, maiores nam voluptates itaque? Maiores voluptates delectus repellat!",
+    tecs: [
+      { tittle: "React", id: 0 },
+      { tittle: "Html", id: 1 },
+      { tittle: "Css", id: 2 },
+    ],
+    links: "https://github.com/Richie735",
+    image: require("../../assets/portfolio4.jpg"),
+  },
 ];
 
-const projectContent = projects.map((project) => (
-  <li className="project">
-    <div className="project-card">
-      <img src={project.image} className="project-img" />
-      <div className="project-content">
-        <div className="project-top">
-          <h3 className="project-title">{project.name}</h3>
-          <div className="project-description">{project.description}</div>
+const projectContent = projects.map((project, i) => (
+  <div count1={i} count2={i + 1}>
+    {i % 2 === 0 ? (
+      <div className="project-container-even">
+        <div className="project-img-cont">
+          <img src={project.image} alt="" className="project-img" />
         </div>
-
-        <ul className="project-list">
-          {project.tecs.map((tech, i) => (
-            <li key={i}>{tech.tittle}</li>
-          ))}
-        </ul>
-        <a href={project.links} target="_blank" rel="noreferrer">
-          <FiGithub />
-        </a>
+        <div className="project-text">
+          <h3>{project.name}</h3>
+          <p>{project.description}</p>
+          <ul className="project-list">
+            {project.tecs.map((tech, i) => (
+              <li key={i}>{tech.tittle}</li>
+            ))}
+          </ul>
+          <a href={project.links} target="_blank" rel="noreferrer">
+            <FiGithub />
+          </a>
+        </div>
       </div>
-    </div>
-  </li>
+    ) : (
+      <div className="project-container-odd">
+        <div className="project-text">
+          <h3>{project.name}</h3>
+          <p>{project.description}</p>
+          <ul className="project-list">
+            {project.tecs.map((tech, i) => (
+              <li key={i}>{tech.tittle}</li>
+            ))}
+          </ul>
+          <a href={project.links} target="_blank" rel="noreferrer">
+            <FiGithub />
+          </a>
+        </div>
+        <div className="project-img-cont">
+          <img src={project.image} alt="" className="project-img" />
+        </div>
+      </div>
+    )}
+  </div>
 ));
 
 function Projects() {
   return (
-    <section id="projects">
+    <section className="projects" id="projects">
       <h2 className="numbered-heading-even">
         <span>04.</span>Projects
       </h2>
-      <ul className="projects-grid">{projectContent}</ul>
+      <div className="projects-grid">{projectContent}</div>
     </section>
   );
 }
